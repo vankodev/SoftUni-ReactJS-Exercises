8b21566f4447b8caec5af8afc8e4de489363dc07
Ivan Ivanov <vankodev@gmail.com>
2023-11-14 01:39:09 +0200

    Add get all games service method

--- games-play-workshop/client/src/services/gameService.js
+++ games-play-workshop/client/src/services/gameService.js
@@ -1,7 +1,17 @@
-const baseUrl = "http://localhost:3030/jsonstore";
+import { request } from "../lib/request";
+
+const baseUrl = "http://localhost:3030/jsonstore/games";
+
+// export const getAll = async () => {
+//     const result = await request("GET", baseUrl);
+
+//     console.log(result);
+
+//     return Object.values(result);
+// };
 
 export const create = async (gameData) => {
-    const response = await fetch(`${baseUrl}/games`, {
+    const response = await fetch(baseUrl, {
         method: "POST",
         headers: {
             "content-type": "application/json",
@@ -12,4 +22,10 @@ export const create = async (gameData) => {
     const result = await response.json();
 
     return result;
+};
+
+export const getAll = async () => {
+    const response = await fetch(baseUrl);
+    const games = await response.json();
+    return Object.values(games);
 };
