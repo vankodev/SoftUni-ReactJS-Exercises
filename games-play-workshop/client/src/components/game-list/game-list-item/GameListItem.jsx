4efca2a45113f8549223ab17a1459ff9b9b772be
Ivan Ivanov <vankodev@gmail.com>
2023-11-14 19:12:54 +0200

    Add game details page

--- games-play-workshop/client/src/components/game-list/game-list-item/GameListItem.jsx
+++ games-play-workshop/client/src/components/game-list/game-list-item/GameListItem.jsx
@@ -1,13 +1,15 @@
-export default function GameListItem({ title, category, imageUrl }) {
+import { Link } from "react-router-dom";
+
+export default function GameListItem({ _id, title, category, imageUrl }) {
     return (
         <div className="allGames">
             <div className="allGames-info">
                 <img src={imageUrl} />
                 <h6>{category}</h6>
                 <h2>{title}</h2>
-                <a href="#" className="details-button">
+                <Link to={`/games/${_id}`} className="details-button">
                     Details
-                </a>
+                </Link>
             </div>
         </div>
     );
