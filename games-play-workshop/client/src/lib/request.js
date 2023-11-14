9a836a55ea36372d4878ad8e7a0a9a0a3c8ec603
Ivan Ivanov <vankodev@gmail.com>
2023-11-14 18:33:25 +0200

    Modify requester

--- games-play-workshop/client/src/lib/request.js
+++ games-play-workshop/client/src/lib/request.js
@@ -1,13 +1,31 @@
-export const request = async (method, url, data) => {
+const buildOptions = (method, data) => {
+    const options = {};
+
+    if (data) {
+        options.body = JSON.stringify(data);
+        options.headers = {
+            "content-type": "application/json",
+        };
+    }
+};
+
+const request = async (method, url, data) => {
     const response = await fetch(url, {
+        ...buildOptions(data),
         method,
     });
 
-    // if (!response.ok) {
-    //     throw new Error('')
-    // }
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
     const result = await response.json();
 
     return result;
 };
+
+export const get = request.bind(null, "GET");
+export const post = request.bind(null, "POST");
+export const put = request.bind(null, "PUT");
+export const remove = request.bind(null, "DELETE");
+export const patch = request.bind(null, "PATCH");
--- games-play-workshop/client/src/services/gameService.js
+++ games-play-workshop/client/src/services/gameService.js
@@ -1,31 +1,15 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const baseUrl = "http://localhost:3030/jsonstore/games";
 
-// export const getAll = async () => {
-//     const result = await request("GET", baseUrl);
+export const getAll = async () => {
+    const result = await request.get(baseUrl);
 
-//     console.log(result);
-
-//     return Object.values(result);
-// };
-
-export const create = async (gameData) => {
-    const response = await fetch(baseUrl, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify(gameData),
-    });
-
-    const result = await response.json();
-
-    return result;
+    return Object.values(result);
 };
 
-export const getAll = async () => {
-    const response = await fetch(baseUrl);
-    const games = await response.json();
-    return Object.values(games);
+export const create = async (gameData) => {
+    const result = await request.post(baseUrl, gameData);
+
+    return result;
 };
